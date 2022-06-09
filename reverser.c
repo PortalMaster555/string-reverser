a3cce9a6405d788e2edc61cdade355c5aeccee7a
PortalMaster555 <portalmaster55@gmail.com>
2022-06-09 08:16:31 -0500

    made the thing print the string

--- reverser.c
+++ reverser.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+#define MAX_STRING_LEN 32
+
+char origString[MAX_STRING_LEN];
+char revString[MAX_STRING_LEN];
 int main (int argc, char **argv)
 {
-	printf("Hello, world!%c", '\n');
+	printf("Input a string to be reversed: ");
+	fgets(origString, MAX_STRING_LEN, stdin);
+	origString[sizeof(origString) - 1] = '\0';
+	printf("The string you typed was %s", origString);
 	return 0;
 }
