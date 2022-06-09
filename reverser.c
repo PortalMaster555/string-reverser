4b4b1a3bfde7b9e73e671d8a14fb1870aab0d854
PortalMaster555 <portalmaster55@gmail.com>
2022-06-09 08:27:24 -0500

    Finalized string reverser is functional

--- reverser.c
+++ reverser.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_STRING_LEN 32
 
@@ -7,8 +8,15 @@ char revString[MAX_STRING_LEN];
 int main (int argc, char **argv)
 {
 	printf("Input a string to be reversed: ");
+	
 	fgets(origString, MAX_STRING_LEN, stdin);
-	origString[sizeof(origString) - 1] = '\0';
-	printf("The string you typed was %s", origString);
+	origString[strlen(origString) - 1] = '\0'; //newline begone
+	
+	printf("The string you typed was \"%s\".%c", origString, '\n');
+	for(int i = 0; i < strlen(origString) - 1; i++)
+	{
+		revString[strlen(origString) - 2 - i] = origString[i];
+	}
+	printf("The reverse of that string is \"%s\".%c", revString, '\n');
 	return 0;
 }
